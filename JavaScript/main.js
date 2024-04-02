bc95f09d8658154f91d3d974e2dac144f001340e
omarAlomar01 <omar454085@gmail.com>
2024-04-02 21:25:42 +0300

    create settings and designed it

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,18 @@
-// yearly target variables
+// set header width dynamically in all files
+function setHeaderWidth() {
+  let bodyWidth = document.body.clientWidth;
+  let sidebarWidth = document.querySelector(".sidebar").offsetWidth;
+  let myHeader = document.querySelector(".content header");
+  let headerWidth = bodyWidth - sidebarWidth;
+  myHeader.style.width = headerWidth + "px";
+}
+setHeaderWidth(); //on page load
+window.addEventListener("resize", () => {
+  //if page width changed or resized
+  setHeaderWidth();
+});
+
+// yearly target variables in index file
 let yearlyTarget = document.querySelector(".yearly-targets");
 let progress = document.querySelectorAll(
   ".yearly-targets .details .progress > span"
@@ -16,3 +30,36 @@ window.onscroll = function () {
     });
   }
 };
+
+// change box border color in settings file
+let inputs = document.querySelectorAll(".social-info .box input");
+let icon = document.querySelectorAll(".social-info .box  i");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("focus", function () {
+    if (this.classList.contains("facebook")) {
+      this.parentNode.style.borderColor = "#1877f2";
+      icon[index].style.borderColor = "#1877f2";
+      icon[index].style.color = "#1877f2";
+    } else if (this.classList.contains("x-twitter")) {
+      this.parentNode.style.borderColor = "#000";
+      icon[index].style.borderColor = "#000";
+      icon[index].style.color = "#000";
+    } else if (this.classList.contains("youtube")) {
+      this.parentNode.style.borderColor = "#f00";
+      icon[index].style.borderColor = "#f00";
+      icon[index].style.color = "#f00";
+    } else if (this.classList.contains("linkedin")) {
+      this.parentNode.style.borderColor = "#0077b5";
+      icon[index].style.borderColor = "#0077b5";
+      icon[index].style.color = "#0077b5";
+    }
+  });
+  input.addEventListener("blur", function () {
+    this.parentNode.style.borderColor = "#ddd";
+    icon.forEach((ele) => {
+      ele.style.borderColor = "#ddd";
+      ele.style.color = "gray";
+    });
+  });
+});
